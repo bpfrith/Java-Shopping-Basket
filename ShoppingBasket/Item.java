5bd1d91ac28053821c42f202bc8c32cf43af170b
Ben Frith <bpfrith@gmail.com>
2017-02-03 17:43:08 +0000

    item class and tests built and working

--- ShoppingBasket/Item.java
+++ ShoppingBasket/Item.java
@@ -2,5 +2,25 @@ package ShoppingBasket;
 import Behaviours.*;
 
 public class Item{
+  private String itemName;
+  private Double price;
+  private int quantity;
+
+  public Item(String itemName, Double cost, int quantity){
+    this.itemName = itemName;
+    this.price = price;
+    this.quantity = quantity;
+  }
+   public String getItemName(){
+    return this.itemName;
+   }
+   
+  public Double getPrice(){
+    return this.price;
+   }
   
+  public int getQuantity(){
+    return this.quantity;
+  }
+ 
 }
--- ShoppingBasketSpecs/ItemTest.java
+++ ShoppingBasketSpecs/ItemTest.java
@@ -2,16 +2,27 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import ShoppingBasket.*;
 
-public class ItemTest{
+public class ItemTest {
+  Item item;
 
   @Before
-  public void before(){
-    item = new Item(10);
+  public void before() {
+    item = new Item("Tooth Brush", 2.60, 1);
   }
 
   @Test
-  public void hasCost(){
-    assertEquals(cost, item.getcost());
+  public void hasItemName(){
+    assertEquals("Tooth Brush", item.getItemName());
   }
-  
+
+  @Test
+  public void hasPrice(){
+    assertEquals(item.getPrice(), 2.60, 0.01);
+  }
+
+  @Test
+  public void hasQuantity(){
+    assertEquals(item.getQuantity(), 1);
+  }
+
 }
